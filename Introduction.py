70e4f2a531cd83143f834037bbe44c01039b1df0
Programmer-govind <117900869+Programmer-govind@users.noreply.github.com>
2022-11-15 20:52:30 +0530

    Add files via upload

---
+++ Introduction.py
@@ -0,0 +1,15 @@
+#Program to give Introduction 
+
+name=input("Enter your name :")
+print("------------------------------------------------------")
+print("Welcome ",name)
+print("My name is Gautam Govind.")
+print("I am a BTech Computer Science Student.")
+print("I love coding and Database Management.")
+print("Currently, I am taking 100 Days Coding Challenge By Anjali Luthra Mam.")
+print("Thank You for visiting.")
+
+print("------------------------------------------------------")
+
+
+
