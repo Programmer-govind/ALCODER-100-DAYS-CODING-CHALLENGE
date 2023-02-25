1c7e12e7ab1212bec22485159e48ec6bf5272605
Gautam Govind <117900869+Programmer-govind@users.noreply.github.com>
2023-02-25 21:16:45 +0530

    Update and rename #Automorphic_Number.py to Automorphic_Number.py

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,4 +1,5 @@
-#Automorphic Number An automorphic number is an integer whose square ends with the given integer
+
+'''Automorphic Number An automorphic number is an integer whose square ends with the given integer'''
 
 num = int(input("Enter a number : "))
 count = len(str(num))
